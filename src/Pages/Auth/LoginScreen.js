06e7cf58fa03192bd246f646ef951d7b78e306b1
Alan Nugraha <yaelahlan@Alans-MacBook-Pro.local>
2020-05-04 22:30:52 +0700

    login register

--- src/Pages/Auth/LoginScreen.js
+++ src/Pages/Auth/LoginScreen.js
@@ -1,21 +1,73 @@
 import React from "react";
 import "./LoginScreen.css";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 export default class Login extends React.Component {
   render() {
     return (
-      <div className="container">
-        <text>Login Screen</text>
-        <Link to={{ pathname: "/forgot-pass" }}>
-          <text>forgot password</text>
-        </Link>
-        <Link to={{ pathname: "/register" }}>
-          <text>register</text>
-        </Link>
-        <Link to={{ pathname: "/" }}>
-          <text>to home</text>
-        </Link>
+      <div className="grid-container-index">
+        {/* <!-- left-section --> */}
+
+        {/* <!-- end of left-section -->
+
+        <!-- right-section --> */}
+        <section className="right-section">
+          <div className="form-header">
+            <header>Welcome to</header>
+            <p>Destinasiku</p>
+          </div>
+          <div className="login-form">
+            <div className="login-form-body">
+              <form action="" method="post">
+                <div className="input-wrapper login">
+                  <div className="input-items">
+                    <input type="email" name="" id="" placeholder="Email Address" />
+                    <label for="">Email Address</label>
+                  </div>
+                  <div className="input-items password">
+                    <input type="password" name="" id="" placeholder="Your password" />
+                    <label for="">Password</label>
+                  </div>
+                </div>
+
+                <div className="forgot-password">
+                  <ul>
+                    <li>
+                      {/* eslint-disable-next-line */}
+                      <a>Forgot Password</a>
+                    </li>
+                  </ul>
+                </div>
+
+                <div className="form-btn">
+                  <ul>
+                    <li>
+                      <button>
+                        Login
+                      </button>
+                    </li>
+                  </ul>
+                </div>
+
+                <div className="flex-footer">
+                  <div className="footer-register">
+                    <p>Don't have an account?</p>
+                  </div>
+
+                  <div className="footer-register">
+                    {/* eslint-disable-next-line */}
+                    <a>Register</a>
+                  </div>
+                </div>
+              </form>
+            </div>
+
+          </div>
+        </section>
+        {/* <!-- end of right-section --> */}
+        <section className="left-section">
+          <img src={require("../../asset/img/login.png")} alt="" />
+        </section>
       </div>
     );
   }
--- src/Pages/Auth/RegisterScreen.js
+++ src/Pages/Auth/RegisterScreen.js
@@ -1,15 +1,68 @@
 import React from "react";
 import "./RegisterScreen.css";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 export default class Register extends React.Component {
   render() {
     return (
-      <div className="container">
-        <text>Register Screen</text>
-        <Link to={{ pathname: "/login" }}>
-          <text>back to login</text>
-        </Link>
+      <div className="grid-container-index">
+        {/* <!-- left-section --> */}
+
+        {/* <!-- end of left-section -->
+
+        <!-- right-section --> */}
+        <section className="right-section">
+          <div className="form-header">
+            <header>Register an account</header>
+            <p>Destinasiku</p>
+          </div>
+          <div className="login-form">
+            <div className="login-form-body">
+              <form action="" method="post">
+                <div className="input-wrapper login">
+                  <div className="input-items">
+                    <input type="email" name="" id="" placeholder="Email Address" />
+                    <label for="">Email Address</label>
+                  </div>
+                  <div className="input-items password">
+                    <input type="password" name="" id="" placeholder="Your password" />
+                    <label for="">Password</label>
+                  </div>
+                  <div className="input-items password">
+                    <input type="password" name="" id="" placeholder="Your password" />
+                    <label for="">Confirm Password</label>
+                  </div>
+                </div>
+
+                <div className="form-btn">
+                  <ul>
+                    <li>
+                      <button>
+                        Register
+                      </button>
+                    </li>
+                  </ul>
+                </div>
+
+                <div className="flex-footer">
+                  <div className="footer-login">
+                    <p>Already have an account?</p>
+                  </div>
+
+                  <div className="footer-login">
+                    {/* eslint-disable-next-line */}
+                    <a>Login</a>
+                  </div>
+                </div>
+              </form>
+            </div>
+
+          </div>
+        </section>
+        {/* <!-- end of right-section --> */}
+        <section className="left-section">
+          <img src={require("../../asset/img/register.png")} alt="" />
+        </section>
       </div>
     );
   }
